1d75942641e679cf68686be658b800155846748a
parimalapg <99467748+parimalapg@users.noreply.github.com>
2023-07-19 12:07:32 +0000

    Added NEW tag

--- main.go
+++ main.go
@@ -168,7 +168,7 @@ func readHandler(w http.ResponseWriter, r *http.Request) {
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
 	t := Todo{}
-	t.Title = r.FormValue("title")
+	t.Title = "NEW: " + r.FormValue("title")
 
 	if len(r.FormValue("complete")) > 0 && r.FormValue("complete") != "false" {
 		t.Complete = true
